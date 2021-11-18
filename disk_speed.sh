5ac18cd0acc45957dc9fe0c6443ee991efb78c1a
Alvin Abad <alvnabad@gmail.com>
2021-11-17 18:40:02 -0800

    Add read speed and sync

--- disk_speed.sh
+++ disk_speed.sh
@@ -2,22 +2,49 @@
 
 unalias -a
 
+rm -f 1G.txt
+sync
+
 ARCH=`uname -ms`
 
 echo Disk Speed Test: $ARCH
 
 case $ARCH in
     "Linux x86_64"):
+        echo "Writing..."
         dd if=/dev/zero of=1G.txt bs=100M count=10 oflag=direct
+        sync
+
+        echo "Reading..."
+        dd if=1G.txt of=/dev/null bs=100M count=10 iflag=direct
+        sync
         ;;
     "Linux armv7l"):
+        echo "Writing..."
         dd if=/dev/zero of=1G.txt bs=100M count=10 oflag=direct
+        sync
+
+        echo "Reading..."
+        dd if=1G.txt of=/dev/null bs=100M count=10 iflag=direct
+        sync
         ;;
     "Darwin x86_64"):
+        echo "Writing..."
         dd if=/dev/zero of=1G.txt bs=100m count=10
+        sync
+
+        echo "Reading..."
+        dd if=/dev/zero of=1G.txt bs=100m count=10
+        sync
         ;;
     "VMkernel x86_64"):
+        echo "Writing..."
         time dd if=/dev/zero of=1G.txt bs=100M count=10
+        sync
+
+        echo "Reading..."
+        time dd of=/dev/null if=1G.txt bs=100M count=10
+        sync
         ;;
     *):
         echo "Unknown machine."
@@ -25,3 +52,4 @@ case $ARCH in
 esac
 
 rm -f 1G.txt
+sync
