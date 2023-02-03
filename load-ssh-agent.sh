2efab1d5cfe9993984b4cd0818a3f43359ca93c8
Alvin Abad <alvnabad@gmail.com>
2023-02-03 05:58:06 -0800

    Fix ssh-agent loading Add usage Add ssh-agent kill option Add log messages

--- load-ssh-agent.sh
+++ load-ssh-agent.sh
@@ -2,46 +2,54 @@
 
 #-------------------------------------------------------------------------------
 # Enable ssh-agent
-# Source this in ~/.profile or ~/.bash_profile
+# Usage: Must be sourced to run
 # Example:
 #     . $HOME/src/unix-tools/load-ssh-agent.sh 72h 
 #
-# Another way to set expiration time, is to set this ENV
-#     export SSH_AGENT_EXP=72h
+# This may be added to ~/.profile or ~/.bash_profile to run upon log in.
 #-------------------------------------------------------------------------------
 
-SSH_AGENT_EXP=${SSH_AGENT_EXP=72h}
+usage() {
+    cat <<EOF
+Usage:
+    load-ssh-agent <expiration>
 
-# if expiration is supplied at the command line
-if [ -n "$1" ]; then
-    SSH_AGENT_EXP=$1
-fi
+expiration:
+    0           No expiration
+    72h         Expire in 72 hours
+    120h        Expire in 120 hours
+    -k          Terminate existing ssh-agent
+EOF
+}
 
-KEY_FILES="
-    $HOME/.ssh/id_rsa
-    $HOME/.ssh/github.pem
-    $HOME/.ssh/id_rsa.socks5
-"
+# set keyfiles to load
+KEY_FILES="${KEY_FILES:=-default}"
 
 SA_DIR=$HOME/.ssh
 SA_FILE=${SA_DIR}/sa
 
+# Create an ssh-agent
 create_ssh_agent() {
     # killall any ssh-agents and create a new one
     killall ssh-agent || true
 
-    echo "Launching new ssh-sgent."
+    echo "Launching a new ssh-sgent."
     mkdir -p $SA_DIR
     chmod 700 $SA_DIR
+    echo "Saving to $SA_FILE"
     ssh-agent -t $SSH_AGENT_EXP > $SA_FILE
+
+    # load ssh-agent info
+    echo "Loading ssh-agent..."
     . $SA_FILE
 
-    for k in $KEY_FILES
+    # load keys to agent
+    echo "Loading keys to agent..."
+    for key in $KEY_FILES
     do
         #ssh-add -t 72h $HOME/.ssh/$k
-        echo ----------------------------------------
-        echo $k
-        ssh-add $k
+        echo ssh-add -t $SSH_AGENT_EXP $key
+        ssh-add -t $SSH_AGENT_EXP $key
     done
 }
 
@@ -49,24 +57,42 @@ create_ssh_agent() {
 # START MAIN
 #-------------------------------------------------------------------------------
 
-# check if SA_FILE exists
-if [ -f "$SA_FILE" ]; then
-    # source SA_FILE
-    . $SA_FILE
+SSH_AGENT_EXP=$1
 
-    if ps -h -f -p $SSH_AGENT_PID && [ -e "$SSH_AUTH_SOCK" ]; then
+# if SA_FILE exists, source it
+if [ $# -eq 0 ]; then
+    usage
+elif [ -f "$SA_FILE" ]; then
+    # source SA_FILE to get ssh-agent PID
+    echo "Found: $SA_FILE loading..."
+    . $SA_FILE > /dev/null
+
+    # if ssh-agent is up
+    if ps -f -p $SSH_AGENT_PID && [ -e "$SSH_AUTH_SOCK" ]; then
+        # if desired to kill agent
+        if [ "$SSH_AGENT_EXP" = "-k" ]; then
+            echo "Killing ssh-agent..."
+            ssh-agent -k
         # if keys have expired, add them
-        if ! ssh-add -l; then
+        elif ! ssh-add -l; then
+            echo "Keys have expired. Adding to agent..."
             for k in $KEY_FILES
             do
-                echo --------------------
-                echo $k
+                echo "    $k"
                 ssh-add $k
             done
         fi
+    # if there is no ssh-agent running, create a new one
     else
         create_ssh_agent
     fi
+# if SA file does not exist, create a new ssh-agent
 else
-    create_ssh_agent
+    echo "No ssh-agent found."
+    if [ "$SSH_AGENT_EXP" = "-k" ]; then
+        echo "Killing ssh-agent..."
+        ssh-agent -k
+    else
+        create_ssh_agent
+    fi
 fi
