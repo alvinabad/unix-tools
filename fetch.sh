d70c634abb9dab3cf94f83f4e610ad78d42f06dc
Alvin Abad <alvnabad@gmail.com>
2025-02-03 02:14:09 -0800

    Add SKIP repos

--- fetch.sh
+++ fetch.sh
@@ -2,10 +2,30 @@
 
 set -e 
 
+SKIP="
+ctvl-tools
+"
+
 for r in `ls`
 do
     [ -d "$r" ] || continue
     [ -d "$r/.git" ] || continue
+
+    found=false
+    for s in $SKIP
+    do
+        #echo "Skip: $s"
+        if [ "$s" = "$r" ]; then
+            found=true
+            #echo "FOUND: $s $r"
+            break
+        fi
+    done
+
+    #echo "Found: $found"
+    if [ "$found" = "true" ]; then
+        continue
+    fi
 
     echo --------------------------------------------------------------------------------
     echo REPO: $r
